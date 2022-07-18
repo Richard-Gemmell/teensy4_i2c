f0fc595e3441889ab1affd505aa1f993aa4b3752
Richard Gemmell <16683467+Richard-Gemmell@users.noreply.github.com>
2022-07-18 14:05:49 +0100

    Added upload_delay.py to make it easier to run automated tests.

---
+++ pio_scripts/upload_delay.py
@@ -0,0 +1,9 @@
+Import("env")
+
+def after_upload(source, target, env):
+    print("Delay while uploading...")
+    import time
+    time.sleep(0.35)
+    print("Done!")
+
+env.AddPostAction("upload", after_upload)
