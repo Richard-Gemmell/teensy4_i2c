364ee2059b360e885490bc0e808f1105fdbb4abb
Richard Gemmell <16683467+Richard-Gemmell@users.noreply.github.com>
2022-07-31 10:33:54 +0100

    Fixed some errors in i2c_driver.h documentation.

--- src/i2c_driver.h
+++ src/i2c_driver.h
@@ -128,24 +128,23 @@ public:
     // useful when the slave is listening on multiple addresses.
     virtual void before_transmit(std::function<void(uint16_t address)> callback) = 0;
 
-    // Sets a callback to be called by the ISR each time
-    // each time the slave has sent a block of data to the master.
+    // Sets a callback to be called by the ISR each time the slave
+    // has sent a block of data to the master.
     // 'address' is the address that the master called. This is only
     // useful when the slave is listening on multiple addresses.
     virtual void after_transmit(std::function<void(uint16_t address)> callback) = 0;
 
     // Determines which data will be sent to the master the next time
     // it reads from us.
-    // The master will receive up to 'numBytes' from us. If it demands
+    // The master will receive up to 'size' bytes from us. If it demands
     // more data it will be sent 0xFF until it ends the request.
-    // The call to onTransmit gives the actual number of bytes transmitted.
     virtual void set_transmit_buffer(uint8_t* buffer, size_t size) = 0;
 
-    // Determines where to put data we receive from the the master
-    // the next it writes to us.
-    // The master can send up to 'numBytes' of data to us. Extra bytes
-    // will be dropped. The call to onReceive gives the actual number
-    // of bytes received.
+    // Determines where to put data we receive from the master
+    // the next time it writes to us.
+    // The master can send up to 'size' bytes of data to us. Extra bytes
+    // will be dropped. The 'after_receive' callback receives the actual number
+    // of bytes received in the 'length' argument.
     virtual void set_receive_buffer(uint8_t* buffer, size_t size) = 0;
 };
 
